3c74831f03c2e319c4112c28ce5a3720c813be05
D3BAS1SH <Morning.Star00.111@protonmail.com>
2023-12-16 03:57:15 +0530

    Added Comments for documentation/Understanding to OrderPair
    Added Comments for Documentation/Understanging to Set

--- MathProjClasses/OrderPair.java
+++ MathProjClasses/OrderPair.java
@@ -2,36 +2,56 @@ package MathProjClasses;
 
 public class OrderPair {
     
+    //To hold values of any pairs
     private String a,b;
 
+    //Initialize Pair with nothing
     public OrderPair(){
         this.a=new String("");
         this.b=new String("");
     }
+
+    //Inititalize Pair with given Characters
     public OrderPair(char a,char b){
         this.a = new String(Character.toString(a));
         this.b = new String(Character.toString(b));
     }
+
+    //Initialize Pair with Given String
     public OrderPair(String a,String b){
         this.a = new String(a);
         this.b = new String(b);
     }
+
+    //Initialize Pair with Given Integers
     public OrderPair(int a,int b){
         this.a = new String(Integer.toString(a));
         this.b = new String(Integer.toString(b));
     }
+
+    //Initialize Pair with Given doubles
     public OrderPair(double a,double b){
         this.a=new String(Double.toString(a));
         this.a=new String(Double.toString(a));
     }
+
+    //To get Value of First Value OF the Pair
     public String getA(){return this.a;}
+
+    //To get Value of Second Value of the Pair
     public String getB(){return this.b;}
+
+    //To Check If two OrderPair are Equal or Not
     public boolean isEqual(OrderPair OP){
         return (this.getA().equals(OP.getA())&&this.getB().equals(OP.getB()));
     }
+
+    //To check if Two OrderPair are Equal or not
     public static boolean isEqual(OrderPair OP0, OrderPair OP1){
         return ((OP0.getA().equals(OP1.getA()))&&(OP0.getB().equals(OP1.getB())));
     }
+
+    //Overriding toString method to print the value of the OrderPair.
     @Override
     public String toString(){
         return "("+this.getA()+","+this.getB()+")";
--- MathProjClasses/Set.java
+++ MathProjClasses/Set.java
@@ -4,25 +4,36 @@ import java.util.ArrayList;
 
 public class Set {
 
+    //List of the Order Pair are to be Fileed in a Set
     private ArrayList<OrderPair> FullSet = new ArrayList<>();
+
+    //List all the Distinct Elements Available in the Above List.
     private ArrayList<String> eachEle = new ArrayList<>();
 
+    //Default Contructor;
     public Set(){
 
     }
+
+    //Set the Values of the OrderPair When Given as a Array to List.
     public Set(OrderPair[] set){
         for(int i=0;i<set.length;i++){
             FullSet.add(set[i]);
         }
+        //Generate All Distinct Elements.
         generateDistinctEls();
     }
+
+    //Set the values of the Orderpair When given as a List to the list.
     public Set(ArrayList<OrderPair> set){
         for(int i=0;i<set.size();i++){
             FullSet.add(set.get(i));
         }
+        //Generate All Distinct Elements.
         generateDistinctEls();
     }
 
+    //Set Get all the Pairs that Starts with a given Value.
     private ArrayList<OrderPair> getOPStartsWith(String V){
         ArrayList<OrderPair> OPS = new ArrayList<>();
         for(int i=0;i<FullSet.size();i++){
@@ -32,6 +43,8 @@ public class Set {
         }
         return OPS;
     }
+
+    //To Check if the Given Order Pair is Available in the List or Not
     private boolean isAvailable(OrderPair V){
         boolean check=false;
         for(int i=0;i<FullSet.size();i++){
@@ -41,6 +54,8 @@ public class Set {
         }
         return check;
     }
+
+    //To Check if the Given Set is Symmetric Or not
     public boolean checkSymmetry(){
         boolean checkAll = true;
         for(int i=0;i<FullSet.size();i++){
@@ -50,6 +65,8 @@ public class Set {
         }
         return checkAll;
     }
+
+    //To Check if Set is Reflexive
     public boolean checkReflexive(){
         boolean checkAll=true;
         for(int i=0;i<eachEle.size();i++){
@@ -61,6 +78,8 @@ public class Set {
         }
         return checkAll;
     }
+
+    //To Check if the Set is Transistive Or Not
     public boolean checkTransitive(){
         boolean checkAll =true;
         for(int i=0;i<FullSet.size();i++){
@@ -74,13 +93,18 @@ public class Set {
         }
         return checkAll;
     }
+
+    //To Check if the Given Set is Equivalence Or not
     public boolean checkEquivalence(){
         return (checkReflexive()&&checkSymmetry()&&checkTransitive());
     }
 
+    //To check if the Given Value is Available in the Distinct Element List.
     private boolean isPresentInEachEle(String V){
         return eachEle.contains(V);
     }
+
+    //To Generate the distinct Element List.
     private void generateDistinctEls(){
         if(eachEle.size()==0){
             for(OrderPair X : FullSet){
@@ -102,6 +126,8 @@ public class Set {
             }
         }
     }
+
+    //To get the array list of the Distinct Elements.
     public ArrayList<String> getDistinctEls(){
         if(eachEle.size()==0){
             generateDistinctEls();
